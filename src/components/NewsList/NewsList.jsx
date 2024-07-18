50864808adf57ed0009fb1f531d52a2e7afc5bc2
Alexandr <chernousov013@gmail.com>
2024-07-18 17:04:16 +0700

    add NewsListItem

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -1,11 +1,11 @@
+import NewsListItem from '../NewsListItem/NewsListItem';
 import styles from './style.module.scss';
-
 
 const NewsList = ({ news }) => {
     return (
         <ul className={styles.list}>
             {news.map((item) => {
-                return <li key={item.id}>{item.title}</li>;
+                return <NewsListItem key={item.id} item={item}/>;
             })}
         </ul>
     );
