1fccfacc0e63d9a9ed0a580f0130659dc24c449b
Alexandr <chernousov013@gmail.com>
2024-07-20 15:17:42 +0700

    add Skeleton

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -3,15 +3,18 @@ import styles from './style.module.scss';
 import NewsBanner from '../../components/NewsBanner/NewsBanner';
 import { getNews } from '../../api/apiNews';
 import NewsList from '../../components/NewsList/NewsList';
+import Skeleton from '../../components/Skeleton.jsx/Skeleton';
 
 const MainPage = () => {
     const [news, setNews] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchNews = async () => {
             try {
                 const res = await getNews();
                 setNews(res.news);
+                setIsLoading(false);
             } catch (error) {
                 console.log(error);
             }
@@ -21,8 +24,12 @@ const MainPage = () => {
 
     return (
         <main className={styles.main}>
-            {news.length > 0 && <NewsBanner item={news[0]} />}
-            <NewsList news={news} />
+            {news.length > 0 && !isLoading ? (
+                <NewsBanner item={news[0]} />
+            ) : (
+                <Skeleton type={'banner'} count={1} />
+            )}
+            {!isLoading ? <NewsList news={news} /> : <Skeleton type={'item'} count={10} />}
         </main>
     );
 };
